451417156c46e2ddf3253c8e2ff95564c50bcc7f
Prakhar314 <54989891+Prakhar314@users.noreply.github.com>
2021-02-01 20:24:41 +0530

    add endpoint to set user claims

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,62 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import admin from './admin';
-import {LocationData} from './status';
+import { LocationData } from './status';
 
 const app: Application = express();
+
+app.use(express.json());// for parsing application/json
+app.use(express.urlencoded({ extended: true }));
 
 let http = require("http").Server(app);
 let io = require("socket.io")(http);
 
+
+admin
+    .auth()
+    .setCustomUserClaims('SwkV5TXNZhRpmPnRmtSpAwfGJmw1', { admin: true })
+    .then(() => {
+        // The new custom claims will propagate to the user's ID token the
+        // next time a new one is issued.
+    });
+
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Hello World');
+});
+
+app.get('/setDriver', (req: Request, res: Response, next: NextFunction) => {
+
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
+        let authToken = req.headers.authorization.split(' ')[1];
+        if (req.body.uid) {
+            admin.auth().verifyIdToken(authToken).then((claims) => {
+                if (claims.admin === true) {
+
+                    admin
+                        .auth()
+                        .setCustomUserClaims(req.body.uid, { driver: true })
+                        .then(() => {
+                            // The new custom claims will propagate to the user's ID token the
+                            // next time a new one is issued.
+                        });
+                }
+                else {
+                    res.sendStatus(401);
+                }
+            })
+                .catch((error) => {
+                    res.status(500).send(error);
+                });
+
+        }
+        else {
+            res.status(400).json('No UID sent');
+        }
+
+    }
+    else {
+        res.sendStatus(401);
+    }
+
 });
 
 
